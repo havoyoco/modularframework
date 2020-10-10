3c3882492279ec6fcd4c27f1c1a20c2612e40c80
havoyoco <72672728+havoyoco@users.noreply.github.com>
2020-10-10 18:15:55 -0500

    Product name was changed to apple care

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "Apple";
+		String product = "Apple care";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
